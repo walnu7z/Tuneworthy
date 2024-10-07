2b4f55c706d3b8f72994131b139bf6fa293f124d
Luis Arredondo <walter@ciencias.unam.mx>
2024-10-06 21:08:51 -0600

    added method in Prospector to list files in specified music Directory

--- src/Minero.py
+++ src/Minero.py
@@ -1,5 +1,6 @@
 from src.Database import *
 import os
+import fnmatch
 
 class Mena:
     def __init__(self, PATH, TPE1, TIT2, TALB, TDRC, TCON, TRCK):
@@ -14,18 +15,30 @@ class Mena:
         self.ID_TALB = None
 
 class Prospector:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, directory = "~/Music"):
+        self.directory = os.path.expanduser(directory)
+        self.mp3_dir_files = []
+
+    def find_mp3_files(self):
+        """
+        Recursively searches through the specified directory and returns a list of all MP3 files found.
+
+        Returns:
+            list: A list of paths to MP3 files.
+        """
+        for dirpath, dirnames, filenames in os.walk(self.directory):
+            self.mp3_dir_files.append([dirpath, []])
+            for filename in fnmatch.filter(filenames, '*.mp3'):
+                self.mp3_dir_files[-1][1].append(filename)
 
 class Minero:
     def __init__(self):
         self.db = MusicLibraryDB()
-
-    def display_info(self):
-        return f"TPE1: {self.TPE1}\nTIT2: {self.TIT2}\nTALB: {self.TALB}\nTDRC: {self.TDRC}\nTCON: {self.TCON}\nTRCK: {self.TRCK}"
 
     def add_entry_from_mena(self, mena: Mena):
         self.db.add_entry(mena.TPE1, mena.TALB, mena.PATH, mena.TIT2, mena.TRCK, mena.TDRC, mena.TCON)
 
     def close_DB(self):
         self.db.close()
+
+    
--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 import sys
+from src.Minero import *
 
 if __name__ == "__main__":
     print("Hello, World!")
+    minero = Minero()
+    
+    pros = Prospector()
+    files = pros.find_mp3_files()
+
+    print(files[-1])
